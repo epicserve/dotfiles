8ecd0776f47a64d5102ce9f6913e7732539ce66a
Brent O'Connor <brent@canopyteam.org>
2025-12-27 12:58:01 -0600

    Install the AWS cli

--- scripts/base_install.sh
+++ scripts/base_install.sh
@@ -5,3 +5,13 @@ if ! command -v uv >/dev/null 2>&1; then
   echo "Installing uv..."
   curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
+
+# install the AWS CLI
+if ! command -v aws >/dev/null 2>&1; then
+  echo "Installing aws..."
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+  unzip /tmp/awscliv2.zip -d /tmp
+  sudo /tmp/aws/install
+  sudo rm /tmp/awscliv2.zip
+  sudo rm -rf /tmp/aws
+fi
