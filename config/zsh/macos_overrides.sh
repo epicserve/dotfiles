b18b9a94f14cd5673d0280b8a2cfc67411c46004
Brent O'Connor <epicserve@gmail.com>
2026-06-16 12:29:04 -0500

    Add SSH_AUTH_SOCK export for 1Password agent in macOS overrides

--- config/zsh/macos_overrides.sh
+++ config/zsh/macos_overrides.sh
@@ -17,3 +17,5 @@ export EDITOR='pycharm'
 # Pycharm
 export PATH="$PATH:/Applications/PyCharm.app/Contents/MacOS"
 
+# Set the SSH_AUTH_SOCK to the 1Password agent socket
+export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
